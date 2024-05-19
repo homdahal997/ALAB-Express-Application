981f61949574df9e8fc5dda0bddeb3620d5dadf9
Hom Dahal <homdahal997@gmail.com>
2024-05-19 07:11:38 -0400

    some modification to express application to display console messages

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+// Import the express module
 const express = require("express");
 
+//Create an instance of an express application
 const app = express();
 
+/// Define a route handler for GET requests made to the root path ('/')
 app.get("/", (req, res) =>{
+    // Send the following view as response to the client
     res.send("Welcome to my own first Express application")
 });
 
-app.listen(3000);
+// Start the application on port 3000
+app.listen(3000, () => {
+    // Log a message to the console when the application starts
+    console.log("The application is running on localhost:3000");
+});

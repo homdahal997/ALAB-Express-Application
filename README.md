5a0ea9e4b733b0b295a5b01d57bf6c1349e14214
Hom Dahal <homdahal997@gmail.com>
2024-05-19 13:38:47 -0400

    Read me file added

---
+++ README.md
@@ -0,0 +1,22 @@
+# Building Express Application
+
+Express Cards is a simple web application built with Node.js and Express. It uses Pug as the template engine.
+
+## Features
+
+- Renders views using Pug.
+- Handles GET requests to the "/" and "/resourcedownload" routes.
+- Serves static images and allows them to be downloaded.
+- Logs request method and URL using a middleware function.
+- Handles POST requests from a form.
+
+## Installation
+
+1. Clone this repository to your local machine.
+2. Navigate to the project directory.
+3. Run `npm install` to install the dependencies.
+
+## Usage
+
+1. Run `node app.js` to start the application.
+2. Open your web browser and navigate to `http://localhost:3000`.
--- app.js
+++ app.js
@@ -13,17 +13,28 @@ app.get("/", (req, res) =>{
     res.render("index")
 });
 
+// This middleware is used to parse URL-encoded bodies. 
+// The "extended: true" option allows parsing of nested objects.
 app.use(express.urlencoded({ extended: true }));
+
+// This middleware serves static files from the 'public' directory.
 app.use(express.static('public'));
 
+// This route handler handles POST requests to "/submit-form".
+// It logs the request body to the console and sends a response to the client.
 app.post("/submit-form", (req, res) => {
     console.log(req.body);
     res.send("successfully submitted");
 });
+
+// This route handler handles GET requests to "/resourcedownload".
+// It renders the 'resourcedownload' view and sends it as a response to the client.
 app.get("/resourcedownload", (req, res) => {
     res.render("resourcedownload")
 });
 
+// This route handler handles GET requests to any route in the form "/:name".
+// It renders a view with the same name as the route parameter and sends it as a response to the client.
 app.get("/:name", (req, res) => {
     res.render(req.params.name);
 });
